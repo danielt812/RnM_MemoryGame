178c0161ab6e7efb2a349e41c0beda113d5e00b0
danielt812 <danielt812@gmail.com>
2018-10-16 04:33:53 -0500

    changed style

--- ricknmorty/src/components/JumboTron/JumboTron.js
+++ ricknmorty/src/components/JumboTron/JumboTron.js
@@ -2,12 +2,15 @@ import React from "react";
 import "./JumboTron.css";
 
 const JumboTron = () => (
-    <div className="jumbotron jumbotron-fluid">
-        <div className="container">
-            <h1 className="display-4">Clicky Game</h1>
-            <p className="lead">Click on an image to earn points, but don't click on any more than once!</p>
-        </div>
+  <div className="jumbotron jumbotron-fluid">
+    <div className="container">
+      <h1 className="display-4">Rick and Morty Memory Game!</h1>
+      <h2 className="lead">
+        Click on a character to earn points, but don't click on the same
+        character more than once!
+      </h2>
     </div>
-)
+  </div>
+);
 
 export default JumboTron;
