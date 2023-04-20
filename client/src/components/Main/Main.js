b2b3e9d21129e0cfda9393851b8795206f4ca23b
danielt812 <danieltolan@gmail.com>
2023-04-19 14:43:54 -1000

    Updates

--- client/src/components/Main/Main.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import './Main.scss';
-
-const Main = (props) => (
-	<div className={props.background}>{props.children}</div>
-);
-
-export default Main;
--- client/src/components/Wrapper/Wrapper.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import './Wrapper.scss';
-
-const Wrapper = (props) => (
-	<div className={`wrapper ${props.background}`}>{props.children}</div>
-);
-
-export default Wrapper;
